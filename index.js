a8b200832271f2ba7c312669f31a4a61ed5aab32
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-06-03 00:28:20 -0400

    Night 1

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var wordToGuessEl = document.getElementById('word-to-guess')
+var previousWordEl = document.getElementById('previous-word')
+var incorrectLettersEl = document.getElementById('incorrect-letters')
+var remainingGuessesEl = document.getElementById('remaining-guesses')
+var scoreEl = document.getElementById('score')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
+
+var wins = 0 
+var losses = 0
+var remainingGuesses = 10
+
+document.onkeyup = function(e) {
+  console.log(e.key)
+}
+
+
+
