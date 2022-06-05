1158b1c99c8d1607f2c3c1e21f86982d36d5795c
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-06-05 18:43:25 -0400

    Underscores, Scores & Key ups

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ var words = [
   'mango'
 ]
 
+// Randomize Words in Array
+var word = words[Math.floor(Math.random()* words.length)];
+console.log(word)
+
+// Get elements
 var wordToGuessEl = document.getElementById('word-to-guess')
 var previousWordEl = document.getElementById('previous-word')
 var incorrectLettersEl = document.getElementById('incorrect-letters')
@@ -22,11 +27,46 @@ var lossesEl = document.getElementById('losses')
 
 var wins = 0 
 var losses = 0
-var remainingGuesses = 10
 
-document.onkeyup = function(e) {
-  console.log(e.key)
+// Place Remaining Guesses on DOM
+var remain = 10
+remainingGuessesEl.innerHTML = remain
+
+// // HAVE TO MOVE THIS DOWN TO BE A FUNCTION WITH TYPE CHOICES*** 
+// document.onkeyup = function(e) {
+//   var key = e.key.toLowerCase()
+//   console.log(e.key)
+// }
+
+// Displaying Underscores
+var correctLetters = []
+for (var i = 0; i < word.length; i++) {
+  correctLetters[i] = "_";
 }
+wordToGuessEl.innerHTML=correctLetters.join("")
+
+// Populating Scores & Correct Letters
+
+// key up with console log of type choice
+document.onkeyup = function(e) {
+  var key = e.key.toLowerCase()
+  console.log(e.key)
+
+if (word.indexOf(key)== -1) {
+  incorrectLettersEl.innerHTML += key
+  remain --
+  remainingGuessesEl.innerHTML= remain
+  console.log("wrong boi!")
+} else {
+  for (var i = 0; i < word.length; i++) {
+    if(word[i]== key)
+    correctLetters[i] = key
+    console.log("dats right! keep going!")
+  }
+}
+wordToGuessEl.innerHTML= correctLetters.join("")
+}
+
 
 
 
